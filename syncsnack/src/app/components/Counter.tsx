cafb882e99aab3e4206eb8836da10495f85260de
Andrija Skontra <askontra22@student.unizd.hr>
2024-08-30 08:35:56 +0200

    add dark mode colors

--- syncsnack/src/app/components/Counter.tsx
+++ syncsnack/src/app/components/Counter.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { Button } from "@chakra-ui/react";
+import { Button, useColorMode } from "@chakra-ui/react";
 import { useTranslations } from "next-intl";
 import React, { useState } from "react";
 
 export default function Counter() {
   const t = useTranslations("HomePage");
   const [number, setNumber] = useState(0);
+  const { colorMode, toggleColorMode } = useColorMode();
 
   function handleClick() {
     setNumber((prew) => prew + 1);
@@ -18,6 +19,9 @@ export default function Counter() {
       </h1>
       <Button colorScheme="xblue" onClick={handleClick}>
         Click
+      </Button>
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === "light" ? "Dark" : "Light"}
       </Button>
     </div>
   );
