7ecec3589f80e656b1dbacae4a0b3ad49aa08485
Šime Rončević <sime.roncevic@kodelab.hr>
2024-08-30 09:56:22 +0200

    Automatically Redirecting user to login page

--- syncsnack/src/app/[locale]/page.tsx
+++ syncsnack/src/app/[locale]/page.tsx
@@ -2,13 +2,9 @@ import {useTranslations} from 'next-intl';
 import {Link} from '@/routing';
 import Counter from '../components/Counter';
  
-export default function RootPage() {
+export default function HomePage() {
   const t = useTranslations('HomePage');
   return (
-    <div>
-      <h1>{t('title')}</h1>
-      <Link href="/about">{t('about')}</Link>
-      <Counter></Counter>
-    </div>
+    <h1>{t('title')}</h1>
   );
 }
