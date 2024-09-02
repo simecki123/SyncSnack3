238c83aba20fed9abfad99cdf736cc6e963420b2
Andrija Skontra <askontra22@student.unizd.hr>
2024-09-02 12:41:21 +0200

    add modal to plus button on sidebar

---
+++ syncsnack/src/app/components/modals/JoinCreateGroupModal.tsx
@@ -0,0 +1,30 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from "@chakra-ui/react";
+
+export default function JoinCreateGroupModal({ isOpen, onClose }: any) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Modal Title</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>hello</ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={onClose}>
+            Close
+          </Button>
+          <Button variant="ghost">Secondary Action</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
