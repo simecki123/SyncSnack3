ae7246018b45a939b4f548ba339f36a09d8268be
Andrija Skontra <askontra22@student.unizd.hr>
2024-09-02 12:52:00 +0200

    add intl to group join create modal

--- syncsnack/src/app/components/modals/JoinCreateGroupModal.tsx
+++ syncsnack/src/app/components/modals/JoinCreateGroupModal.tsx
@@ -8,15 +8,34 @@ import {
   ModalCloseButton,
   Button,
 } from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { useTranslations } from "next-intl";
 
 export default function JoinCreateGroupModal({ isOpen, onClose }: any) {
+  const t = useTranslations("GroupJoinCreateModal");
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
+        <ModalHeader>{t("Header")}</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>hello</ModalBody>
+        <ModalBody>
+          <Tabs isFitted variant="enclosed">
+            <TabList mb="1em">
+              <Tab>{t("Tab1")}</Tab>
+              <Tab>{t("Tab2")}</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <p>one!</p>
+              </TabPanel>
+              <TabPanel>
+                <p>two!</p>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </ModalBody>
 
         <ModalFooter>
           <Button colorScheme="blue" mr={3} onClick={onClose}>
